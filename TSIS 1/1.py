c4848ef00d229ff1e825f1a92dbd6394b36966c8
oltoir <altair.zhanseitov@gmail.com>
2022-06-19 16:25:02 +0600

    first task

--- TSIS 1/1.py
+++ TSIS 1/1.py
@@ -2,5 +2,25 @@
 # 64 names and you are looking
 # for a value in it using the
 # binary search method.
-# What is the maximum number of checks it can take?
+# What is the maximum number of checks it can take? 6
 
+arr = []
+with open('data.txt', 'r') as f:
+    for i in range (64):
+        arr.append(f.readline()[:-1][3:])
+
+# function for binary search to find a name in sorted array
+def binary_search(arr, name):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == name:
+            return mid
+        elif arr[mid] < name:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+print(binary_search(arr, 'Zack'))
