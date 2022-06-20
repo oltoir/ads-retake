f841f046806c2b2da76216d6e2e31dad4b4879b8
Abdurahim Kenesbekov <78414696+kenesbekov@users.noreply.github.com>
2022-06-20 18:37:31 +0600

    case study

---
+++ TSIS 1/8.py
@@ -0,0 +1,34 @@
+# Write code for binary search (preferably in Python).
+# It should take 1) a list (consisting of numbers) and 2) a certain desired number to search for.
+# It should give the correct answer: the number of iterations passed in the binary search.
+
+import random
+
+
+def binary_search(arr, number):
+    if sorted(arr) != arr:
+        arr = sorted(arr)
+    cnt = 0
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        cnt += 1
+        mid = (low + high) // 2
+        if arr[mid] == number:
+            print(number, "at", mid, "\nNumber of checks:", cnt)
+            return mid
+        elif arr[mid] < number:
+            low = mid + 1
+        else:
+            high = mid - 1
+    print(number, "wasn't found\nNumber of checks:", cnt)
+    return -1
+
+
+random_list = []
+for i in range(0, 64):
+    number = random.randint(0, 100)
+    random_list.append(number)
+
+searching_number = 21
+binary_search(random_list, searching_number)
